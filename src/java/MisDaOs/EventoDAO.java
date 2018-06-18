189bae3d32a5cd5ad21df2225c0a07a9c1066b99
sebai <sebai@DESKTOP-G04B92L>
2018-06-18 15:51:39 -0400

    -

--- src/java/MisDaOs/EventoDAO.java
+++ src/java/MisDaOs/EventoDAO.java
@@ -1,5 +1,11 @@
 package MisDaOs;
+import MisClases.Amigo;
+import MisClases.Evento;
+import java.util.List;
 
 public interface EventoDAO {
-    
+    boolean agregarEvento(Evento evento);
+    void eliminarEvento(String nombreEvento);
+    String burcarEvento(String nombreEvento);
 }
+
